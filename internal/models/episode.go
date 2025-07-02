8d582a28c7f7f33d892bd95d4ca7ba0fc53a2a9b
Collin Pendleton <collinp@collinpendleton.com>
2025-07-01 21:29:16 -0500

    Added a couple more shows

--- internal/models/episode.go
+++ internal/models/episode.go
@@ -38,8 +38,8 @@ func GetImagePathForShow(showName string) string {
 		return "/static/images/shows/wonderful.jpg"
 	} else if strings.Contains(showNameLower, "besties") {
 		return "/static/images/shows/besties.jpg"
-	} else if strings.Contains(showNameLower, "bunker") {
-		return "/static/images/shows/bunker-buddies.jpg"
+	} else if strings.Contains(showNameLower, "furious") {
+		return "/static/images/shows/fast-and-furious-and-justin-and-sydnee.jpg"
 	} else if strings.Contains(showNameLower, "cool") && strings.Contains(showNameLower, "games") {
 		return "/static/images/shows/coolgames-inc.jpg"
 	} else if strings.Contains(showNameLower, "empty") {
